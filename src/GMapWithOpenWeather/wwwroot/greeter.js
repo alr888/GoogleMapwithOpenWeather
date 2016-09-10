d888ed5c301efcab15af303fb0ed7b6ed59f70d3
alr888 <ariel.l.reyes@gmail.com>
2016-09-11 08:55:10 +1200

    Updated version with TypeScript

---
+++ src/GMapWithOpenWeather/wwwroot/greeter.js
@@ -0,0 +1,15 @@
+var Greeter = (function () {
+    function Greeter(greeting) {
+        this.greeting = greeting;
+    }
+    Greeter.prototype.greet = function () {
+        return this.greeting;
+    };
+    return Greeter;
+}());
+;
+var objToday = new Date(), weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'), dayOfWeek = weekday[objToday.getDay()], curHour = objToday.getHours() > 12 ? objToday.getHours() - 12 : (objToday.getHours() < 10 ? "0" + objToday.getHours() : objToday.getHours()), curMinute = objToday.getMinutes() < 10 ? "0" + objToday.getMinutes() : objToday.getMinutes(), curMeridiem = objToday.getHours() > 12 ? "p.m." : "a.m.";
+var today = dayOfWeek + " @ " + curHour + ":" + curMinute + " " + curMeridiem;
+var greeter = new Greeter("It's a " + today + " Care to check the weather?");
+document.getElementById('id_time').innerHTML = greeter.greet();
+//# sourceMappingURL=greeter.js.map
---
+++ src/GMapWithOpenWeather/wwwroot/greeter.ts
@@ -0,0 +1,19 @@
+﻿class Greeter {
+    constructor(public greeting: string) { }
+    greet() {
+        return this.greeting;
+    }
+};
+
+var objToday = new Date(),
+    weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
+    dayOfWeek = weekday[objToday.getDay()],
+    curHour = objToday.getHours() > 12 ? objToday.getHours() - 12 : (objToday.getHours() < 10 ? "0" + objToday.getHours() : objToday.getHours()),
+    curMinute = objToday.getMinutes() < 10 ? "0" + objToday.getMinutes() : objToday.getMinutes(),
+    curMeridiem = objToday.getHours() > 12 ? "p.m." : "a.m.";
+var today = dayOfWeek + " @ " + curHour + ":" + curMinute + " " + curMeridiem;
+
+var greeter = new Greeter("It's a " + today + " Care to check the weather?");
+
+document.getElementById('id_time').innerHTML = greeter.greet();
+
